93245ed4584b970776c5e38325484d260c02eb47
devvick <devvick230385@gmail.com>
2022-09-02 22:26:24 +0100

    created screens components

---
+++ src/components/shared/ListTile.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ListTile = () => {
+  return (
+    <div>
+      <div className="shadow p-3 my-2 cursor-pointer">
+        <p className="text-blue-500">Burna boy</p>
+      </div>
+      <div className="shadow p-3 my-2 cursor-pointer">
+        <p className="text-blue-500">Burna boy</p>
+      </div>
+    </div>
+  );
+};
+
+export default ListTile;
---
+++ src/components/video/Video.js
@@ -0,0 +1,28 @@
+import React from "react";
+import image from "../../assets/img.jpeg";
+
+const Video = ({ onClick, isLoading }) => {
+  return (
+    <div
+      onClick={onClick}
+      className="aspect-w-1 group cursor-pointer aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8"
+    >
+      <img
+        alt=""
+        src={image}
+        style={{ width: "100%", height: "40vh", objectFit: "cover" }}
+        className={`
+            "duration-700 ease-in-out group-hover:opacity-75 object-cover",
+            ${
+              isLoading
+                ? "scale-110 blur-2xl grayscale"
+                : "scale-100 blur-0 grayscale-0"
+            }
+          `}
+        //   onLoadingComplete={() => setLoading(false)}
+      />
+    </div>
+  );
+};
+
+export default Video;
