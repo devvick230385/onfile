375a8550357c3e8dc0f786ace269e74e765831d3
devvick <devvick230385@gmail.com>
2022-09-02 22:28:15 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
+import AudiosScreen from "./screens/audios/AudiosScreen";
+import DocumentsScreen from "./screens/documents/DocumentsScreen";
+import ImagesScreen from "./screens/images/ImagesScreen";
+import VideosScreen from "./screens/videos/VideoScreen";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/images" element={<ImagesScreen />} />
+      <Route path="/videos" element={<VideosScreen />} />
+      <Route path="/audios" element={<AudiosScreen />} />
+      <Route path="/documents" element={<DocumentsScreen />} />
+    </Routes>
+  );
 };
 
 export default App;
